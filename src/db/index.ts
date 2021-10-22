262d5b3a91e98f4fa0cef08fb9730440b28bad2f
Lizard Morrow <lizardmorrow@gmail.com>
2021-10-22 15:56:28 +0100

    Add types to db/index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,14 @@
-import { PoolClient } from 'pg';
+import { PoolClient, QueryResult } from 'pg';
 
 const { Pool } = require('pg');
 const pool: PoolClient = new Pool();
 
 module.exports = {
-  query: (text, params, callback) => {
+  query: (
+    text: string,
+    params: Array<string>,
+    callback: (err: Error, result: QueryResult<any>) => void
+  ) => {
     return pool.query(text, params, callback);
   },
 };

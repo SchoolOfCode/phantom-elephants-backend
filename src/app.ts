093fb2dea3a6da7bde206768663243ac76070310
Lizard Morrow <lizardmorrow@gmail.com>
2021-11-09 12:26:45 +0000

    Enable all cors requests

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: '*' }));
+app.use(cors());
 
 // console.log(bootcampRouter);
 // console.log(recordRouter);
